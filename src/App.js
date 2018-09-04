7784cfedfa08b222e1307b1be7e1c85b288e6f32
Tyler Collier <tyler.collier+github@devmounta.in>
2018-09-03 23:43:08 -0700

    Prove that redux and react router still work with this hot reloading

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import { Route, Link } from 'react-router-dom';
+
+class SomeComponent extends Component {
+  render() {
+    return <div>
+      Global counter: {this.props.globalCounter}
+      <button onClick={this.props.increment}>Click to increment global counter</button>
+    </div>
+  }
+}
+
+const ConnectedSomeComponent = connect(state => ({ globalCounter: state.globalCounter }), {
+  increment: () => ({ type: 'INCREMENT' })
+})(SomeComponent)
 
 class App extends Component {
   state = {
@@ -32,6 +47,22 @@ class App extends Component {
           counter: {this.state.counter}
           <div><button onClick={() => this.setState({ counter: this.state.counter + 1 })}>Click me2</button></div>
         </div>
+        <Route exact path="/" render={() => {
+          return <div>
+            <h1>Home</h1>
+            <ConnectedSomeComponent />
+          </div>
+        }} />
+        <Route path="/something" render={() => {
+          return <div>
+            <h1>Something route</h1>
+            <ConnectedSomeComponent />
+          </div>
+        }} />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/something">Something</Link>
+        </nav>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 // Add this import:
 import { AppContainer } from 'react-hot-loader';
+
+const store = createStore((state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, globalCounter: state.globalCounter + 1 };
+    default:
+      return state;
+  }
+}, {
+  globalCounter: 0
+});
 
 // Wrap the rendering in a function:
 const render = () => {
   ReactDOM.render(
     // Wrap App inside AppContainer
     <AppContainer>
+      <Provider store={store}>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
+    </Provider>
     </AppContainer>,
     document.getElementById('root')
   );

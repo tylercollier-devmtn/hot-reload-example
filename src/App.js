4a4f8b73ecf3b5678d1f4a7c46966424d315838c
Tyler Collier <tyler.collier+github@devmounta.in>
2018-09-03 22:24:28 -0700

    Add axios and example debug configuration on port 3000 instead of 8080

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  componentDidMount() {
+    axios.get('https://apis.devmountain.com/gods/', {
+      headers: {
+        apikey: 'tylercollier'
+      }
+    }).then(response => {
+      console.log('-------------- response.data', response.data);
+    })
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +28,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          counter: {this.state.counter}
+          <div><button onClick={() => this.setState({ counter: this.state.counter + 1 })}>Click me</button></div>
+        </div>
       </div>
     );
   }

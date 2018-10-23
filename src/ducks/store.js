27c46ef425df74f5a6a02776baaad0f5b1be432c
Tyler Collier <tyler.collier+github@devmounta.in>
2018-10-23 15:05:42 -0700

    Redux store state now honors hot reload

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,4 +1,19 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-export default createStore(reducer);
+// See: https://github.com/facebook/create-react-app/issues/2317#issuecomment-303081658
+const configureStore = () => {
+  const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept("./reducer", () => {
+        store.replaceReducer(reducer)
+      })
+    }
+  }
+
+  return store
+}
+
+export default configureStore
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './ducks/store';
+import configureStore from './ducks/store';
 import { BrowserRouter } from 'react-router-dom';
 
 // Add this import:
 import { AppContainer } from 'react-hot-loader';
+
+const store = configureStore()
 
 // Wrap the rendering in a function:
 const render = () => {
